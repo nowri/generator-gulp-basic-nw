cdba2ff8175d87b074f5a14407c8930c996a8b5a
nowri <interactive@nowri.in>
2015-06-04 20:30:43 +0900

    Add all

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,54 +4,24 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the wonderful ' + chalk.red('GulpBasicNw') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
-  },
-
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
     },
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
+    writing: {
+        app: function () {
+            this.fs.copy(
+                this.templatePath('gulp-basic-nw/'),
+                this.destinationPath('./')
+            );
+            this.fs.copy(
+                this.templatePath('gulp-basic-nw/**/.*'),
+                this.destinationPath('./')
+            );
+        }
+    },
 
-  install: function () {
-    this.installDependencies();
-  }
+    install: function () {
+        this.installDependencies();
+    }
 });
